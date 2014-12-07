e9643cf0079d1457229f6f5d0036a238a6eae309
Peter Hsieh <peter14f@hotmail.com>
2014-12-07 11:01:55 +0800

    Initial setup for Java/133-clone_graph/Solution.java

---
+++ Java/133-clone_graph/Solution.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    class UndirectedGraphNode {
+        int label;
+        List<UndirectedGraphNode> neighbors;
+
+        UndirectedGraphNode(int x) { 
+            label = x; 
+            neighbors = new ArrayList<UndirectedGraphNode>(); 
+        }
+    }
+
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
