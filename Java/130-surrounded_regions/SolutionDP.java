05914f472707140e86255f58d404adec971c6c20
Peter Hsieh <peter14f@gmail.com>
2015-12-08 12:36:45 +0800

    130-surrounded_region pass OJ. The recursive solution won't pass the large test case. I'm doing blob analysis.

---
+++ Java/130-surrounded_regions/SolutionDP.java
@@ -0,0 +1,21 @@
+
+public class SolutionDP {
+
+    public void solve(char[][] board) {
+        int m = board.length;
+        
+        if (m < 1)
+            return;
+        
+        int n = board[0].length;
+        
+    }
+    
+    public static void main(String[] args) {
+        SolutionDP sol = new SolutionDP();
+        
+        
+
+    }
+
+}
