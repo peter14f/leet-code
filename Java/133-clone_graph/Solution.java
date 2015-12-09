401f72d560fc5c9a5567ccb3e589976c4eac39fc
Peter Hsieh <peter14f@gmail.com>
2015-12-09 20:56:24 +0800

    133-clone_graph pass OJ. This is an O(n) space solution.

--- Java/133-clone_graph/Solution.java
+++ Java/133-clone_graph/Solution.java
@@ -1,68 +1,28 @@
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Solution {
     
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-
         if (node==null)
             return null;
         
-        // toVisit contains nodes in the src graph
-        ArrayList<UndirectedGraphNode> toVisit = new ArrayList<UndirectedGraphNode>();
-        toVisit.add(node);
+        HashMap<UndirectedGraphNode, UndirectedGraphNode> mapping = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        dfsCopy(node, mapping);
         
-        // map of allocated nodes
-        HashMap<Integer, UndirectedGraphNode> newNodeMap = 
-                new HashMap<Integer, UndirectedGraphNode>();
-        
-        UndirectedGraphNode startNode = null;
-        
-        while (!toVisit.isEmpty()) {
-            node = toVisit.remove(0);
-            
-            UndirectedGraphNode newNode;
-            
-            if (newNodeMap.containsKey(node.label)) {
-                // this node was already allocated
-                newNode = newNodeMap.get(node.label);
-            }
-            else {
-                // node allocation needed
-                newNode = new UndirectedGraphNode(node.label);
-                
-                if (newNodeMap.isEmpty()) {
-                    // start start of the graph which will be returned
-                    startNode = newNode;
-                }
-                
-                newNodeMap.put(newNode.label, newNode);
-            }
-
-            for (UndirectedGraphNode neighbor: node.neighbors) {
-                
-                if (newNodeMap.containsKey(neighbor.label)) {
-                    // neighbor node already deep copied
-                    newNode.neighbors.add(newNodeMap.get(neighbor.label));
-                }
-                else {
-                    // neighbor node needs to be deep copied
-                    UndirectedGraphNode newNeighbor = new UndirectedGraphNode(neighbor.label);
-                    newNodeMap.put(newNeighbor.label, newNeighbor);
-                    newNode.neighbors.add(newNodeMap.get(neighbor.label));
-                    
-                    toVisit.add(neighbor);
-                }
-            }
+        return mapping.get(node);
+    }
+    
+    private void dfsCopy(UndirectedGraphNode node, HashMap<UndirectedGraphNode, UndirectedGraphNode> mapping) {
+        if (mapping.containsKey(node)) {
+            return;
         }
         
-        return startNode;
-    }
-
-    // print all neighbor nodes
-    public void printNeighbors(UndirectedGraphNode node) {
-        for (UndirectedGraphNode n : node.neighbors) {
-            System.out.println(" neighbor " + n.label);
+        UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
+        mapping.put(node, newNode);
+                
+        for (UndirectedGraphNode neighbor: node.neighbors) {            
+            dfsCopy(neighbor, mapping);
+            newNode.neighbors.add(mapping.get(neighbor));
         }
     }
     
@@ -82,28 +42,6 @@ public class Solution {
         two.neighbors.add(zero);
         two.neighbors.add(one);
         two.neighbors.add(two);
-        
-        System.out.println("-----Graph A-----");
-        System.out.println("node " + zero.label);
-        sol.printNeighbors(zero);
-        
-        for (UndirectedGraphNode n : zero.neighbors) {
-            System.out.println("node " + n.label);
-            sol.printNeighbors(n);
-        }
-        
-        UndirectedGraphNode a = sol.cloneGraph(zero);
-        
-        System.out.println("-----Graph B-----");
-        System.out.println("node " + a.label);
-        sol.printNeighbors(a);
-        
-        for (UndirectedGraphNode n : a.neighbors) {
-            System.out.println("node " + n.label);
-            sol.printNeighbors(n);
-        }
-        
-        
     }
 
 }
