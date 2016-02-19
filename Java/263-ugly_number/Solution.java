d25ea787593643a70008b7b344ecb4277bc51d26
Peter Hsieh <peter14f@gmail.com>
2016-02-19 01:00:38 -0800

    263-ugly_number passes OJ.

---
+++ Java/263-ugly_number/Solution.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+
+public class Solution {
+
+    public boolean isUgly(int num) {
+        if (num < 1)
+            return false;
+        
+        while ((num % 2) == 0){
+            num = num / 2;
+        }
+        
+        while ((num % 3) == 0){
+            num = num / 3;
+        }
+        
+        while ((num % 5) == 0){
+            num = num / 5;
+        }
+        
+        return (num == 1);
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        boolean isUgly = sol.isUgly(14);
+        System.out.println(isUgly);
+    }
+
+}
