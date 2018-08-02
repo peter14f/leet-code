efe54a9c37796f701f347ce45acd681a5dd43385
Peter Hsieh <peter14f@gmail.com>
2018-08-01 19:23:39 -0700

    421-maximum_xor_of_two_numbers_in_an_array

--- Java/421-maximum_xor_of_two_numbers_in_an_array/Solution.java
+++ Java/421-maximum_xor_of_two_numbers_in_an_array/Solution.java
@@ -35,10 +35,83 @@ public class Solution {
         return maxXor;
     }
 
+    public int findMaximumXOR2(int[] nums) {
+        Trie trie = new Trie();
+
+        for (int num : nums) {
+            trie.insert(num);
+        }
+
+        int max = 0;
+        for (int num: nums) {
+            int XOR = trie.maxXORWith(num);
+            max = Math.max(XOR, max);
+        }
+        return max;
+    }
+
+    static class Trie {
+        TrieNode root;
+        public Trie() {
+            this.root = new TrieNode();
+        }
+        public void insert(int x) {
+            TrieNode cur = root;
+            for (int i=31; i>=0; i--) {
+                if ((x & (1 << i)) > 0) {
+                    // bit is 1
+                    if (cur.one == null) {
+                        cur.one = new TrieNode();
+                    }
+                    cur = cur.one;
+                } else {
+                    // bit is 0
+                    if (cur.zero == null) {
+                        cur.zero = new TrieNode();
+                    }
+                    cur = cur.zero;
+                }
+            }
+        }
+
+        public int maxXORWith(int x) {
+            TrieNode cur = root;
+            int xor = 0;
+            for (int i=31; i>=0; i--) {
+                if ((x & (1 << i)) > 0) {
+                    // bit is 1, want to go to zero if possible
+                    if (cur.zero != null) {
+                        cur = cur.zero;
+                        xor = xor | (1<<i);
+                    } else {
+                        cur = cur.one;
+                    }
+                } else {
+                    // bit is 0, want to go to one if possible
+                    if (cur.one != null) {
+                        cur = cur.one;
+                        xor = xor | (1<<i);
+                    } else {
+                        cur = cur.zero;
+                    }
+                }
+            }
+            return xor;
+        }
+    }
+
+    static class TrieNode {
+        TrieNode zero;
+        TrieNode one;
+        public TrieNode() {
+            
+        }
+    }
+
     public static void main(String[] args) {
         int[] nums = {14, 11, 7, 2};
         Solution sol = new Solution();
-        int maxXOR = sol.findMaximumXOR(nums);
+        int maxXOR = sol.findMaximumXOR2(nums);
         System.out.println(maxXOR);
     }
 
